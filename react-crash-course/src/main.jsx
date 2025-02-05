db2c200923281b043d0ae9bd65127f16a6eb8af5
Tri Nguyen <triminh.nguyen2098@gmail.com>
2025-02-05 21:05:43 +0700

    first commit

---
+++ react-crash-course/src/main.jsx
@@ -0,0 +1,35 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Posts, {loader as postsLoader} from './routes/Posts.jsx';
+import NewPost, {action as newPostAction} from './routes/NewPost.jsx';
+import './index.css';
+import RootLayout from './routes/RootLayout.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Posts />,
+        loader: postsLoader,
+        children: [
+          {
+            path: '/create-post',
+            element: <NewPost />,
+            action: newPostAction
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
